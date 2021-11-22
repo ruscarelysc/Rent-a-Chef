600403a6b79f33c6e564af873888d37fcf31c210
Ruscarelys Canizalez <ruscarelys@gmail.com>
2021-11-22 16:55:31 +1100

    chef_controller done

--- app/controllers/chef_controller.rb
+++ app/controllers/chef_controller.rb
@@ -1,18 +1,38 @@
 class ChefController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
-
-  def show
+  before_action :set_chef, only: [:show, :destroy]
+  def index
+    @chefs = Chef.all
   end
 
-  def index
+   def show
   end
 
   def new
+    @chef = Chef.new
   end
 
   def create
+    @chef = Chef.new(chef_params)
+    if @chef.save
+      redirect_to chef_path(@chef)
+    else
+      render "new"
+    end
   end
 
   def destroy
+    @chef.destroy
+    redirect_to chefs_path(@chef)
+  end
+
+  private
+
+  def set_chef
+    @chef = Chef.find(params[:id])
+  end
+
+  def chef_params
+    params.require(:chef).permit(:name, :category, :price)
   end
 end
