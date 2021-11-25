14c5f3e8cc94fcaa9fa28b71f367899ace39ff64
Ruscarelys Canizalez <ruscarelys@gmail.com>
2021-11-25 11:14:54 +1100

    add photo to chefs table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_24_001726) do
+ActiveRecord::Schema.define(version: 2021_11_24_235735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_11_24_001726) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "photo"
     t.index ["user_id"], name: "index_chefs_on_user_id"
   end
 
